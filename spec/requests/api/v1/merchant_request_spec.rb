e83eaba13396393311e983b9327d22cc8b7b0297
Drew Proebstel <d.proebstel@gmail.com>
2022-07-14 15:55:01 -0700

    get mercahnt by id rspec

--- spec/requests/api/v1/merchant_request_spec.rb
+++ spec/requests/api/v1/merchant_request_spec.rb
@@ -1,19 +1,33 @@
 require 'rails_helper'
 
 describe "Merchant API" do
-  it "sends a list of merchants" do
-        create_list(:merchant, 3)
+  it "get all" do
+    create_list(:merchant, 3)
 
-        get '/api/v1/merchants'
+    get '/api/v1/merchants'
 
-        expect(response).to be_successful
+    expect(response).to be_successful
 
-        merchants = JSON.parse(response.body, symbolize_names: true)
-        expect(merchants[:data].count).to eq(3)
+    merchants = JSON.parse(response.body, symbolize_names: true)
+    expect(merchants[:data].count).to eq(3)
 
-        merchants[:data].each do |merchant|
-          expect(merchant[:attributes]).to have_key(:name)
-          expect(merchant[:attributes][:name]).to be_a(String)
-        end
-      end
+    merchants[:data].each do |merchant|
+      expect(merchant[:attributes]).to have_key(:name)
+      expect(merchant[:attributes][:name]).to be_a(String)
+    end
   end
+
+  it "get one" do
+    merchants = create_list(:merchant, 3)
+
+    get "/api/v1/merchants/#{merchants.last.id}"
+
+    expect(response).to be_successful
+
+    merchant = JSON.parse(response.body, symbolize_names: true)
+
+    expect(merchant[:data]).to be_a(Hash)
+    expect(merchant[:data][:attributes]).to have_key(:name)
+    expect(merchant[:data][:attributes][:name]).to be_a(String)
+  end
+end
